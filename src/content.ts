4845816dc19e767d270667718ebfe3dbceb75a2f
woorld <17719812+woorld@users.noreply.github.com>
2024-04-19 03:29:54 +0900

    ポスト展開時に前回右ペインを閉じた位置に戻ってしまうのを抑制する処理を追加

--- src/content.ts
+++ src/content.ts
@@ -18,6 +18,15 @@ observer.observe(document, {
   subtree: true
 });
 
+const invalidScrollAmount = 200;
 document.addEventListener('scroll', () => {
+  // 1回のスクロール量が一定より多い場合は戻す
+  // （ポスト展開時に前回右ペインを閉じたときの位置に戻ってしまうのを抑制）
+  if (window.scrollY <= currentScrollY - invalidScrollAmount
+    || window.scrollY >= currentScrollY + invalidScrollAmount) {
+    window.scrollTo(0, currentScrollY);
+    return;
+  }
+
   currentScrollY = window.scrollY;
 });
