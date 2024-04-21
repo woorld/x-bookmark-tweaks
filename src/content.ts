b4ed017e315e2a87babc63c4cd9f87d81ae9beed
woorld <17719812+woorld@users.noreply.github.com>
2024-04-21 20:26:32 +0900

    ブックマークページ以外では動作しないよう修正

--- src/content.ts
+++ src/content.ts
@@ -1,25 +1,11 @@
-// TODO: ブックマークページでのみ動作するようにする（manifest.jsonで指定すると別ページからの遷移で動作しなくなる）
 let currentScrollY = 0;
 let currentUrl = location.href;
 
-// documentを監視
-const observer = new MutationObserver(() => {
-  if (currentUrl === location.href) {
-    return;
-  }
-
-  // URLが以前から変更されていた場合、スクロール位置を復元
-  currentUrl = location.href;
-  window.scrollTo(0, currentScrollY);
-});
-
-observer.observe(document, {
-  childList: true,
-  subtree: true
-});
-
 const invalidScrollAmount = 200;
-document.addEventListener('scroll', () => {
+const bookmarkPageUrl = 'https://twitter.com/i/bookmarks';
+const isBookmarkPage = (url: string): boolean => url.startsWith(bookmarkPageUrl);
+
+function onScroll(): void {
   // 1回のスクロール量が一定より多い場合は戻す
   // （ポスト展開時に前回右ペインを閉じたときの位置に戻ってしまうのを抑制）
   if (window.scrollY <= currentScrollY - invalidScrollAmount
@@ -29,4 +15,37 @@ document.addEventListener('scroll', () => {
   }
 
   currentScrollY = window.scrollY;
+};
+
+// documentを監視
+const observer = new MutationObserver(() => {
+  if (location.href === currentUrl) {
+    return;
+  }
+
+  const isEnterBookmarkPage = !isBookmarkPage(currentUrl) && isBookmarkPage(location.href);
+  if (isEnterBookmarkPage) {
+    console.log('enabled');
+    document.addEventListener('scroll', onScroll);
+  }
+
+  const isLeaveBookmarkPage = isBookmarkPage(currentUrl) && !isBookmarkPage(location.href);
+  if (isLeaveBookmarkPage) {
+    console.log('disabled');
+    document.removeEventListener('scroll', onScroll);
+  }
+
+  currentUrl = location.href;
+
+  // URLが以前から変更されていた場合、スクロール位置を復元
+  window.scrollTo(0, currentScrollY);
 });
+
+observer.observe(document, {
+  childList: true,
+  subtree: true
+});
+
+if (isBookmarkPage(location.href)) {
+  document.addEventListener('scroll', onScroll);
+}
